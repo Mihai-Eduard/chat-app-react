9f241347e26a1364e7f58655344df91f297ddbef
Eduard Alex Mihai <E.Mihai@student.tudelft.nl>
2023-07-17 00:30:31 +0300

    delete all the style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Header</header>
+    <div>
+      <header>Header</header>
     </div>
   );
 }
