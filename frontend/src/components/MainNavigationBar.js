c22e22431e47313327cd7a17bbcc05d1aa144c58
Eduard Alex Mihai <E.Mihai@student.tudelft.nl>
2023-07-17 16:27:50 +0300

    chagne the navigation bar

--- frontend/src/components/MainNavigationBar.js
+++ frontend/src/components/MainNavigationBar.js
@@ -1,51 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import logo from "../resources/logo192.png";
+import { Link } from "react-router-dom";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
 
 const MainNavigationBar = () => {
   return (
     <header>
-      <nav
-        className="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark"
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary"
+        bg="dark"
         data-bs-theme="dark"
       >
-        <div className="container-fluid">
-          <div className="navbar-brand">
+        <Container fluid>
+          <Navbar.Brand href="#">
             <img
+              alt="logo"
               src={logo}
-              alt="Logo"
               width="30"
-              height="24"
-              className="d-inline-block align-text-top"
+              height="30"
+              className="d-inline-block align-top"
             />
-            Sorinel Piticu
-          </div>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <div className="nav-link active" aria-current="page">
-                  Item1
-                </div>
-              </li>
-              <li className="nav-item">
-                <div className="nav-link active" aria-current="page">
-                  Item2
-                </div>
-              </li>
-              <li className="nav-item">
-                <div className="nav-link active" aria-current="page">
-                  Item3
-                </div>
-              </li>
-            </ul>
-            <Link to="/login">
-              <button className="btn btn-outline-light" type="submit">
-                Login
-              </button>
-            </Link>
-          </div>
-        </div>
-      </nav>
+            {"SorinCord"}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1" style={{ marginLeft: "1rem" }}>
+                Description
+              </Nav.Link>
+              <Nav.Link href="#action2" style={{ marginLeft: "1rem" }}>
+                Features
+              </Nav.Link>
+              <Nav.Link href="#action3" style={{ marginLeft: "1rem" }}>
+                About Us
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Link to="/login">
+                <Button variant="outline-success">Login</Button>
+              </Link>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
